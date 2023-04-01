395fa0e2eb3466348d9dcd03effff72975a3f478
Zatosee <99033119+Zatosee@users.noreply.github.com>
2023-04-01 17:51:47 +0200

    Repair of nav mobile

--- js/anim.js
+++ js/anim.js
@@ -32,19 +32,28 @@ document.querySelectorAll(".carousel").forEach((carousel) => {
 const hamburger = document.querySelector(".menu_hamburger");
 const navLinks = document.querySelector(".nav_links");
 const navLinksList = document.querySelectorAll(".nav_links ul li a");
+const burgerIcon = document.getElementById("burger");
 const image = document.getElementById("burger");
+const body = document.querySelector("body");
 
-hamburger.addEventListener("click", () => {
+burgerIcon.addEventListener("click", () => {
     navLinks.classList.toggle("open");
-    image.src = "img/logos/whitecross.png";
+    if (navLinks.classList.contains("open")) {
+        burgerIcon.src = "img/logos/whitecross.png";
+        body.style.overflowY = "hidden";
+    } else {
+        burgerIcon.src = "img/logos/white-menuburger.png";
+        body.style.overflowY = "scroll";
+    }
 });
 
-navLinksList.forEach((link) => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("open");
-        image.src = "img/logos/white-menuburger.png";
-    });
+// Ajoutez le code suivant pour réinitialiser l'icône lorsque l'on clique sur un lien :
+navLinks.addEventListener("click", () => {
+    burgerIcon.src = "img/logos/white-menuburger.png";
+    navLinks.classList.remove("open");
+    body.style.overflowY = "scroll";
 });
+
 
 window.addEventListener("scroll", function() {
     var header = document.querySelector("nav");
@@ -52,14 +61,24 @@ window.addEventListener("scroll", function() {
 });
 
 function changeImage() {
-    var image = document.getElementById('burger');
-    if (image.src.match("burger")) {
-        image.src = "img/logos/whitecross.png";
+    var burger = document.getElementById('burger');
+
+    document.body.classList.toggle("lock-scroll");
+
+    if (burger.src.match("burger")){
+        burger.src = "img/logos/white-menuburger.png";
     }
+
     else {
-        image.src = "img/logos/white-menuburger.png";
+        burger.src = "img/logos/whitecross.png";
     }
 }
+
+function leavescrollblockonul() {
+    document.body.classList.remove("lock-scroll");
+}
+
+
 
 
 
