34f88ab922cc0e7843aea55d8b2c0e47346c7158
Zatosee <99033119+Zatosee@users.noreply.github.com>
2023-04-02 14:30:06 +0200

    Carousel rectification

--- js/anim.js
+++ js/anim.js
@@ -1,33 +1,63 @@
-document.querySelectorAll(".carousel").forEach((carousel) => {
+// Sélection de tous les éléments de carousel sur la page
+const carousels = document.querySelectorAll(".carousel");
+
+// Pour chaque carousel, on crée les boutons de navigation
+carousels.forEach((carousel) => {
     const items = carousel.querySelectorAll(".carousel__item");
     const buttonsHtml = Array.from(items, () => {
         return '<span class="carousel__button"></span>';
-});
-
+    });
     carousel.insertAdjacentHTML("beforeend", `
-        <div class="carousel__nav">
-        ${buttonsHtml.join("")}
-        </div>
-    `);
-
+    <div class="carousel__nav">
+      ${buttonsHtml.join("")}
+    </div>
+  `);
 
     const buttons = carousel.querySelectorAll(".carousel__button");
+    let currentSlideIndex = 0;
 
+    // On ajoute un écouteur d'événement sur chaque bouton de navigation
     buttons.forEach((button, i) => {
-       button.addEventListener("click", () => {
-           items.forEach((item) => item.classList.remove("carousel__item--selected"));
-           buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+        button.addEventListener("click", () => {
+            // On retire la classe "carousel__item--selected" de l'élément courant
+            items[currentSlideIndex].classList.remove("carousel__item--selected");
+            buttons[currentSlideIndex].classList.remove("carousel__button--selected");
 
-           items[i].classList.add("carousel__item--selected");
-           button.classList.add("carousel__button--selected");
+            // On met à jour l'indice de l'élément courant
+            currentSlideIndex = i;
 
-       });
+            // On ajoute la classe "carousel__item--selected" à l'élément cliqué
+            items[currentSlideIndex].classList.add("carousel__item--selected");
+            buttons[currentSlideIndex].classList.add("carousel__button--selected");
+        });
     });
 
-    items[0].classList.add("carousel__item--selected");
-    buttons[0].classList.add("carousel__button--selected");
+    // On ajoute la classe "carousel__item--selected" au premier élément
+    items[currentSlideIndex].classList.add("carousel__item--selected");
+    buttons[currentSlideIndex].classList.add("carousel__button--selected");
 
+    // Ajout d'un écouteur d'événement sur le carousel pour gérer le swipe sur mobile
+    let touchStartX = 0;
+    let touchEndX = 0;
+
+    carousel.addEventListener("touchstart", (e) => {
+        touchStartX = e.touches[0].clientX;
+    });
+
+    carousel.addEventListener("touchend", (e) => {
+        touchEndX = e.changedTouches[0].clientX;
+
+        // Si le déplacement horizontal est suffisant, on change de slide
+        if (touchEndX - touchStartX > 50 && currentSlideIndex > 0) {
+            buttons[currentSlideIndex - 1].click();
+        } else if (touchStartX - touchEndX > 50 && currentSlideIndex < items.length - 1) {
+            buttons[currentSlideIndex + 1].click();
+        }
+    });
 });
+
+
+/* Menu */
 
 const hamburger = document.querySelector(".menu_hamburger");
 const navLinks = document.querySelector(".nav_links");
@@ -55,10 +85,13 @@ navLinks.addEventListener("click", () => {
 });
 
 
+/* Scroll */
 window.addEventListener("scroll", function() {
     var header = document.querySelector("nav");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+/* Scroll menu */
 
 function changeImage() {
     var burger = document.getElementById('burger');
@@ -74,57 +107,9 @@ function changeImage() {
     }
 }
 
+/* Scroll menu */
+
 function leavescrollblockonul() {
     document.body.classList.remove("lock-scroll");
 }
 
-// Carousel :
-
-$(function() {
-    var carousel = $(".carousel"),
-        items = carousel.find(".carousel__item"),
-        buttons = carousel.find(".carousel__button"),
-        currentIdx = 0;
-
-    // Initialisation du carousel
-    items.eq(currentIdx).addClass("carousel__item--selected");
-    buttons.eq(currentIdx).addClass("carousel__button--selected");
-
-    // Détecter les gestes tactiles
-    carousel.swipe({
-        swipeLeft: function() {
-            if (currentIdx < items.length - 1) {
-                items.eq(currentIdx).removeClass("carousel__item--selected");
-                buttons.eq(currentIdx).removeClass("carousel__button--selected");
-                currentIdx++;
-                items.eq(currentIdx).addClass("carousel__item--selected");
-                buttons.eq(currentIdx).addClass("carousel__button--selected");
-            }
-        },
-        swipeRight: function() {
-            if (currentIdx > 0) {
-                items.eq(currentIdx).removeClass("carousel__item--selected");
-                buttons.eq(currentIdx).removeClass("carousel__button--selected");
-                currentIdx--;
-                items.eq(currentIdx).addClass("carousel__item--selected");
-                buttons.eq(currentIdx).addClass("carousel__button--selected");
-            }
-        },
-        threshold: 50
-    });
-});
-
-
-
-
-
-// let popup = document.querySelector(".popup");
-// function openPopup() {
-//     popup.classList.add("open-popup");
-//     console.log("open");
-// }
-//
-// function closePopup() {
-//     popup.classList.remove("open-popup");
-// }
-//
